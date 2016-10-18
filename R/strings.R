764cb86f837419a8fe3792e1d467bd49fe26d083
Rory Nolan <rorynoolan@gmail.com>
2016-10-18 20:55:45 +0100

    Add RemoveQuoted.

--- R/strings.R
+++ R/strings.R
@@ -612,3 +612,20 @@ LocateBraces <- function(string) {
                 locations, braces, SIMPLIFY = FALSE)
   dfs
 }
+
+#' Remove the quoted parts of a string.
+#'
+#' If any parts of a string are quoted (between quotation marks), remove those parts of the string, including the quotes. Run the examples and you'll know exactly how this function works.
+#'
+#' @param string A character vector.
+#'
+#' @return A character vector.
+#' @examples
+#' string <- "\"abc\"67a\'dk\'f"
+#' cat(string)
+#' RemoveQuoted(string)
+RemoveQuoted <- function(string) {
+  string <- str_replace_all(string, "(?:\".*?\")", "")
+  string <- str_replace_all(string, "(?:\'.*?\')", "")
+  string
+}
